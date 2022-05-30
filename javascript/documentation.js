946b50433f0631279cd318a4b8d654d97af23e5b
Anurag Sharma <anuragsharma0711@gmail.com>
2022-05-30 21:38:55 +0530

    refac:removed console.log

--- javascript/documentation.js
+++ javascript/documentation.js
@@ -1,13 +1,11 @@
-const docs = document.querySelector(".docs")
-const dropdown = document.querySelector(".dropdown")
-const anchorTag = document.querySelectorAll(".anchor-tag")
+const docs = document.querySelector(".docs");
+const dropdown = document.querySelector(".dropdown");
+const anchorTag = document.querySelectorAll(".anchor-tag");
 
-docs.addEventListener('click',(()=>{
-    dropdown.classList.toggle("hide");
-}))
+docs.addEventListener("click", () => {
+  dropdown.classList.toggle("hide");
+});
 
-document.body.addEventListener('click',((e)=>{
-    console.log("hello");
-    if(e.target.className !== "docs")
-     dropdown.classList.add("hide"); 
-})); 
+document.body.addEventListener("click", (e) => {
+  if (e.target.className !== "docs") dropdown.classList.add("hide");
+});
